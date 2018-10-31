41b131f842243be78d22cd44aa1bc6f3e5895aaa
akasoul <antonvoloshuk@gmail.com>
2018-10-31 15:05:32 +0300

    11005

--- client.py
+++ client.py
@@ -2,11 +2,11 @@ import socket
 import numpy as np
 
 
-data=np.array([65535.1213,100.7867,2000.5545])
+data=np.array([1.1,1.2,2.1,2.2,3.1,3.2])
 #np.array2
 sock = socket.socket()
 sock.connect(('localhost', 9090))
-for i in range(0,3):
+for i in range(0,6):
     sock.send(data[i,])
     ans = sock.recv(1024)
     print(ans)
--- server.py
+++ server.py
@@ -5,20 +5,23 @@ import struct
 sock = socket.socket()
 sock.bind(('', 9090))
 
-_array=np.empty(shape=[3,1],dtype=np.float)
+_array=np.empty(shape=[3,2],dtype=np.float)
 while True:
     sock.listen(1)
     conn, addr = sock.accept()
     print('connected:', addr)
     npdata=np.zeros(shape=[3,2])
     for i in range(0,3):
-        data=conn.recv(sys.getsizeof(float))
-        if  data:
-            print(type(data))
-            msg=struct.unpack_from('d',
-                              data)
-            print(msg)
-            _array[i]=msg
-            conn.send(b'odne')
+        for j in range(0,2):
+            data=conn.recv(sys.getsizeof(float))
+            if  data:
+                print(type(data))
+                msg=struct.unpack_from('d',
+                                  data)
+                print(msg)
+
+                _array[i][j]=msg[0]
+                conn.send(b'odne')
     print(_array)
+
 conn.close()
