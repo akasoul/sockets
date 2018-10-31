8312aa4c2c0fe0b702cda9cf8f2e4376b46ec449
akasoul <antonvoloshuk@gmail.com>
2018-10-31 12:41:07 +0300

    11005

--- client.py
+++ client.py
@@ -1,10 +1,14 @@
 import socket
+import numpy as np
 
+
+data=np.array([65535.1213,100.7867,2000.5545])
+#np.array2
 sock = socket.socket()
 sock.connect(('localhost', 9090))
-sock.send('hello, world!')
-
-data = sock.recv(1024)
+for i in range(0,3):
+    sock.send(data[i,])
+    ans = sock.recv(1024)
+    print(ans)
 sock.close()
 
-print(data)
--- server.py
+++ server.py
@@ -1,16 +1,24 @@
 import socket
-
+import numpy as np
+import sys
+import struct
 sock = socket.socket()
 sock.bind(('', 9090))
-sock.listen(1)
-conn, addr = sock.accept()
 
-print('connected:', addr)
 
 while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.send(data.upper())
+    sock.listen(1)
+    conn, addr = sock.accept()
+    print('connected:', addr)
+    npdata=np.zeros(shape=[3,2])
+    for i in range(0,3):
+        data=conn.recv(sys.getsizeof(float))
+        if  data:
+            #data2=np.frombuffer(data,dtype=np.uint8)
+            print(type(data))
+            msg=struct.unpack_from('d',
+                              data)
+            print(msg)
+            conn.send(b'odne')
 
 conn.close()
